7e2d7867791fcf2d865b90ad91cca57d59b62948
jzybert <jmzybert@gmail.com>
2023-06-20 12:10:32 -0700

    Adding in React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './app.css';
+
+import React from 'react';
+import HomePage from './components/HomePage';
+
+function App() {
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>I'm Jonathan Zybert, front-end engineer</h1>
+            <h2>Interested in space exploration, D&D, reading, hiking, and art</h2>
+        </div>
+    );
+};
+
+export default HomePage;
